0b47c09abc4bbcdeb668e42782d6c0125f7fc470
Gabriel da Silva Pereira <gabriel-xp288@hotmail.com>
2025-04-29 20:34:20 -0300

    feat: add Dockerfile and start script

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM php:8.1-fpm
+
+RUN apt-get update && apt-get install -y \
+    zip unzip git curl libzip-dev \
+    && docker-php-ext-install pdo pdo_mysql zip
+
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+WORKDIR /var/www
+
+COPY . .
+
+RUN composer install
+
+EXPOSE 80
+
+CMD ["php-fpm"]
---
+++ setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "mounting app and db containers..."
+docker-compose up -d --build
+
+echo "waiting for MySQL container..."
+until docker-compose exec db mysql -uroot -proot -e "SELECT 1;" &> /dev/null
+do
+  echo -n "."
+  sleep 1
+done
+
+echo "creating databases..."
+docker-compose exec db mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS db_interview_tray;"
+docker-compose exec db mysql -uroot -proot -e "CREATE DATABASE IF NOT EXISTS db_interview_tray_test;"
+
+echo "reset cache..."
+docker-compose exec app php artisan config:clear
+docker-compose exec app php artisan cache:clear
+
+echo "migrations..."
+docker-compose exec app php artisan migrate --database=mysql
+docker-compose exec app php artisan migrate --database=mysql_testing
+
+echo "seeders..."
+docker-compose exec app php artisan db:seed --database=mysql
+
+echo "tests..."
+docker-compose exec app php artisan test
+
+echo "Script finalizado!"
